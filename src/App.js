ebe716c64c5cd6d829407845a89204a03c90f185
Joe <josephfmck@gmail.com>
2021-05-05 21:53:57 -0500

    END 6, add comments to src/App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+//logo img
+import logo from "./logo.svg";
+//main css for every component
+import "./App.css";
 
+//MAIN APP COMPONENT is render through #root
+//Components can be functions (like here) OR Classes
+//Class component: can have state
+//function component: need hooks for state
 function App() {
+  //class for JS, className for JSX
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          Hello World
         </a>
       </header>
     </div>
