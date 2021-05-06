bffb2638788a5ab9c5f3f993e0ad864e730b1592
Joe <josephfmck@gmail.com>
2021-05-06 15:52:22 -0500

    change functional component to class component using extends and render() method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+//* import React so extends work for class
+import React from "react";
 //main css for every component
 import "./App.css";
 
-//MAIN APP COMPONENT is render through #root
+//*MAIN APP COMPONENT is render through #root
 //Components can be functions (like here) OR Classes
 //Class component: can have state
 //function component: need hooks for state
-function App() {
-  //class for JS, className for JSX
-  return (
-    <div className='App'>
-      <h1>Hello from React</h1>
-    </div>
-  );
+
+//changed func to class
+//*For class to work need to extend with React component
+class App extends React.Component {
+  //*class needs render() method to return
+  //lifecycle method
+  render() {
+    //class for JS, className for JSX
+    return (
+      <div className='App'>
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
