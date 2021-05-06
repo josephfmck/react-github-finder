ecf5902f830512fe21f2f923fd9e184f1c5b4cd5
Joe <josephfmck@gmail.com>
2021-05-05 21:38:30 -0500

    comments to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//react framework library
+import React from "react";
+//renders to browser DOM
+import ReactDOM from "react-dom";
+import "./index.css";
+//Main App component - App.js
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+//render method - takes in WHAT you want to render <App /> and WHERE to render "root"
+//<React.StrictMode> is for highlighting potential problems
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
