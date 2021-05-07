a13c08d126fe2e8d6af7bcea88888a82390d47d5
Joe <josephfmck@gmail.com>
2021-05-07 16:27:43 -0500

    Use shorthand ES7 React snippet: rce  to create preset class component

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+//ES7 React snippets Extension shortcut
+//* shorthand:    rce ~press enter~
+//?creates this class component
+import React, { Component } from "react";
+
+export class Navbar extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Navbar;
