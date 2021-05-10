32376271d8081effa2c870980dc9e90489b16d41
Joe <josephfmck@gmail.com>
2021-05-10 14:53:50 -0500

    Changed UserItem.js Class Component to a Functional Component; removing {component} import, render(), and changing this.props.user to props.user

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,40 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 
-//* dont need export because its below already
-class UserItem extends Component {
-  //?REPLACED state with this.props.user TO GRAB Users.js state
-  // //*obj to track data
-  // state = {
-  //   id: "id",
-  //   login: "mojombo",
-  //   avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-  //   html_url: "https://github.com/mojombo",
-  // };
-  //? dont have to use {this.state} in render()
+//* Switch from Class to Function Component
+//* Functional Component: Doesn't need render(), Don't use this.props to grab props, instead pass in (props)
+const UserItem = (props) => {
+  //*Destructure Users.js props
+  const { login, avatar_url, html_url } = props.user;
 
-  render() {
-    //*Destructure Users.js props
-    const { login, avatar_url, html_url } = this.props.user;
-
-    return (
-      <div className='card text-center'>
-        {/* grabs state */}
-        <img
-          src={avatar_url}
-          alt=''
-          className='round-img'
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a
-            href={html_url}
-            className='btn btn-dark btn-sm my-1'
-          >{`${html_url}`}</a>
-        </div>
+  return (
+    <div className='card text-center'>
+      {/* grabs state */}
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          className='btn btn-dark btn-sm my-1'
+        >{`${html_url}`}</a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default UserItem;
