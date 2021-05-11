a970d41eebf32c448aa17e452d72808f7adfc792
Joe <josephfmck@gmail.com>
2021-05-11 14:51:39 -0500

    Users.js turn to Function Component since no State, import Spinner.js, Pass in users and loading Props, IF statement to display <Spinner/> when loading and display users divs when done loading

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 //* grab to add props to this component, Users.js is a parent/wrapper to UserItem
 import UserItem from "./UserItem";
+//* grab Spinner, Users.js is Spinner's wrapper
+import Spinner from "../layout/Spinner";
 
-export class Users extends Component {
-  //?REPLACE State with API data Props passed in from App.js
-  // state = {
-  //   users: [
-  //     {
-  //       id: "id",
-  //       login: "mojombo",
-  //       avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-  //       html_url: "https://github.com/mojombo",
-  //     },
-  //   ],
-  // };
-
-  //? REPLACE {this.state} with {this.props} to grab Users.js API props from App.js
-  render() {
+//?Removed State so change to Functional
+//* Destructure props App.js passed in
+const Users = ({ users, loading }) => {
+  //* IF loading render Spinner, IF done loading render divs
+  if (loading) {
+    return <Spinner />;
+  } else {
+    //? REPLACE {this.state} with {this.props} to grab Users.js API props from App.js, Destructure REPLACE this.props.users to users
+    //? Class to Functional: Remove render()
     return (
       <div style={userStyle}>
         {/* loop create user divs */}
-        {this.props.users.map((u) => (
+        {users.map((u) => (
           // key attr for unique check
           // pass in u as a prop called user
           //*Userprop for each
@@ -29,7 +25,7 @@ export class Users extends Component {
       </div>
     );
   }
-}
+};
 
 //* Declare inline styles, This will be a wrapper grid
 const userStyle = {
