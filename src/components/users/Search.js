ebe040ab0b9da9eb97aeca73018ffc36e5d78214
Joe <josephfmck@gmail.com>
2021-05-11 16:24:47 -0500

    SearchForm needs State to Grab Input's Values, Set search input's value to State's text, NOW CANT TYPE, Add onChange Event to input, Create onChange Event Func that setState({ text: e.target.value }) when onChange text input Fires, NOW CAN TYPE

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 
 export class Search extends Component {
+  //* When using a Form, Want to attach State to inputs
+  state = {
+    text: "",
+  };
+
+  //*OnChange Event, to type into search input
+  onChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
   render() {
     return (
       <div>
         <form className='form'>
-          <input type='text' name='text' placeholder='Search Users...' />
+          {/* set text val to state, NEED onchange event to change text/state  */}
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input
             type='submit'
             value='Search'
