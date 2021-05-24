2ec78428b6a43ee88ec242456c7aedda9cdb514c
Joe <josephfmck@gmail.com>
2021-05-23 19:57:14 -0500

    REFACTOR <Search/> Class to Func, Remove component import, change class to func, move proptypes outside of comp, add const to events, move props to comps params and destructure props, remove render(), replace state with useState() setting text to

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,84 +1,78 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-export class Search extends Component {
-  //* When using a Form, Want to attach State to inputs
-  state = {
-    text: "",
-  };
-
-  //?Class Needs static for propTypes
-  static propTypes = {
-    searchUsersMethodAppJSProp: PropTypes.func.isRequired,
-    clearUsersMethodAppJSProp: PropTypes.func.isRequired,
-    showClearBtnBoolProp: PropTypes.bool.isRequired,
-    setAlertMethodAppJSProp: PropTypes.func.isRequired,
-  };
+const Search = ({
+  searchUsersMethodAppJSProp,
+  showClearBtnBoolProp,
+  clearUsersMethodAppJSProp,
+}) => {
+  //Destructure State,    text=state setText=method to change state
+  // set text to "" with useState, NOW we can use
+  const [text, setText] = useState("");
 
   //* EVENTS
   //*OnChange Event, type into search input
   //? if have lots of inputs: name, email, etc.
   //* Use name attr to set all [e.target.name], [] to dynamic update object properties
-  onChange = (e) => {
+  const onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
-  //* Submit Event
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     //* Check search text not empty, light type alert
     if (this.state.text === "") {
-      //* setAlert() prop from <Search/> in <App/>
+      //* setAlert() <Search/> prop from <App/>
       this.props.setAlertMethodAppJSProp("Please enter something", "light");
     } else {
       console.log(`Search Form Text ${this.state.text}`);
-      // PASS this.state.text UP to <App/>.
-      //* GRABING/USING <App/>'s method
-      //TODO In <App/>: <Search searchUsers={this.searchUsersMethodAppJS}/>
-      //*passing UP this.state.text? as a Prop
+      //*exec with this.state.text
       this.props.searchUsersMethodAppJSProp(this.state.text);
 
-      //clear state / form input's text
+      //clear form input's text
       this.setState({ text: "" });
     }
   };
 
-  render() {
-    //* Destructure return's <Search/> props
-    const { showClearBtnBoolProp, clearUsersMethodAppJSProp } = this.props;
+  return (
+    <div>
+      {/* onSubmit Event exec onSubmit() */}
+      <form onSubmit={this.onSubmit} className='form'>
+        {/* set text val to state, NEED onchange event to change text/state  */}
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={this.state.text}
+          onChange={this.onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {/* sending this clear method UP to <App/> */}
+      {/* ternary if showClear true render btn */}
+      {showClearBtnBoolProp && (
+        <button
+          className='btn btn-light btn-block'
+          onClick={clearUsersMethodAppJSProp}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
-    return (
-      <div>
-        {/* onSubmit Event exec onSubmit() */}
-        <form onSubmit={this.onSubmit} className='form'>
-          {/* set text val to state, NEED onchange event to change text/state  */}
-          <input
-            type='text'
-            name='text'
-            placeholder='Search Users...'
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type='submit'
-            value='Search'
-            className='btn btn-dark btn-block'
-          />
-        </form>
-        {/* sending this clear method UP to <App/> */}
-        {/* ternary if showClear true render btn */}
-        {showClearBtnBoolProp && (
-          <button
-            className='btn btn-light btn-block'
-            onClick={clearUsersMethodAppJSProp}
-          >
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+//Refact: move propTypes outside
+Search.propTypes = {
+  searchUsersMethodAppJSProp: PropTypes.func.isRequired,
+  clearUsersMethodAppJSProp: PropTypes.func.isRequired,
+  showClearBtnBoolProp: PropTypes.bool.isRequired,
+  setAlertMethodAppJSProp: PropTypes.func.isRequired,
+};
 
 export default Search;
